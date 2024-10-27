51c9be5eec5e1653a38274656e44f9dc95d2f38d
eivindmelleby <37309827-eivindmelleby@users.noreply.replit.com>
2024-10-27 01:37:33 +0000

    Agent step completed: Create a settings page and move trips/projects management there

---
+++ migrations/versions/d002f699eef1_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""merge multiple heads
+
+Revision ID: d002f699eef1
+Revises: 4718e0bfa470
+Create Date: 2024-10-27 01:36:15.900599
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd002f699eef1'
+down_revision = '4718e0bfa470'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
