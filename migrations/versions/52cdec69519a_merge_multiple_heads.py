8f088de6c68c7d5db0f410e44ca5d2bd96ef9904
eivindmelleby <37309827-eivindmelleby@users.noreply.replit.com>
2024-10-29 15:20:38 +0000

    Agent query: Please verify if the add expense page shows fuel-specific fields (kilometers, fuel type, fuel amount) when selecting a fuel category and if the CO2 emissions calculation updates when changing fuel amount or type?

---
+++ migrations/versions/52cdec69519a_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""merge multiple heads
+
+Revision ID: 52cdec69519a
+Revises: ff3e6479022f
+Create Date: 2024-10-29 15:20:06.894155
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '52cdec69519a'
+down_revision = 'ff3e6479022f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
