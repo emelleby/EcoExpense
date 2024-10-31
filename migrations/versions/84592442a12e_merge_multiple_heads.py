a836531fb8e04099b961ce14f3ecec8bc9054c97
eivindmelleby <37309827-eivindmelleby@users.noreply.replit.com>
2024-10-31 10:51:11 +0000

    Agent step completed: Fix JavaScript errors in add_expense.html related to fuel expense calculations

---
+++ migrations/versions/84592442a12e_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""merge multiple heads
+
+Revision ID: 84592442a12e
+Revises: 6d77e58cc669
+Create Date: 2024-10-31 10:49:47.049805
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '84592442a12e'
+down_revision = '6d77e58cc669'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
