9be3af84c20cbbc1d38832cb91ca4b15a4ed1cd7
eivindmelleby <37309827-eivindmelleby@users.noreply.replit.com>
2024-10-31 11:05:39 +0000

    Agent query: Can you verify if the summary cards now use a grid layout with a lighter background color and if they properly adjust based on screen size?

---
+++ migrations/versions/7514fcb21c1d_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""merge multiple heads
+
+Revision ID: 7514fcb21c1d
+Revises: 30f2e01a168f
+Create Date: 2024-10-31 11:00:45.582164
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7514fcb21c1d'
+down_revision = '30f2e01a168f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
