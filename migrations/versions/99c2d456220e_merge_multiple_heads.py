3a37f8727237fccf4e1e9162792d449d21b15990
eivindmelleby <37309827-eivindmelleby@users.noreply.replit.com>
2024-10-31 11:35:42 +0000

    Agent query: Can you verify if selecting different trips and projects in the Analysis page updates both the summary cards and charts correctly?

---
+++ migrations/versions/99c2d456220e_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""merge multiple heads
+
+Revision ID: 99c2d456220e
+Revises: 6b3fdca2aad8
+Create Date: 2024-10-31 11:35:12.230576
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '99c2d456220e'
+down_revision = '6b3fdca2aad8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
