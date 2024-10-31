61432ea62d1c7e972a195c84c8f21739b1da616b
eivindmelleby <37309827-eivindmelleby@users.noreply.replit.com>
2024-10-31 10:36:06 +0000

    Agent query: Please test these scenarios: 1. Add a car expense with distance and verify amount calculation 2. Add a fuel expense and verify CO2 emissions calculation Do both calculations update correctly when changing values?

---
+++ migrations/versions/02292876e523_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""merge multiple heads
+
+Revision ID: 02292876e523
+Revises: e5bbb6c2d255
+Create Date: 2024-10-31 10:29:57.775115
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '02292876e523'
+down_revision = 'e5bbb6c2d255'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
