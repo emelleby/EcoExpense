627565086ae900d573d0044cbb155a22f1a262df
eivindmelleby <37309827-eivindmelleby@users.noreply.replit.com>
2024-10-31 10:05:43 +0000

    Agent query: Please test the following scenarios: 1. Add a fuel expense: Enter fuel amount in liters and verify CO2 emissions calculation 2. Add a car distance-based expense: Enter kilometers and fuel consumption, verify both amount and emissions calculations

---
+++ migrations/versions/e5bbb6c2d255_add_kwh_column_to_expense_model.py
@@ -0,0 +1,24 @@
+"""add_kwh_column_to_expense_model
+
+Revision ID: e5bbb6c2d255
+Revises: 46599494ecce
+Create Date: 2024-10-31 10:04:16.410832
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e5bbb6c2d255'
+down_revision = '46599494ecce'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
