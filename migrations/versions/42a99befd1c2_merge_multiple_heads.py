a14a0ff467ee233e87f8832e68c0fa902f6a96a2
Eivind Melleby <37309827-eivindmelleby@users.noreply.replit.com>
2024-10-31 11:26:21 +0000

    Add co2e to Analysis

---
+++ migrations/versions/42a99befd1c2_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""merge multiple heads
+
+Revision ID: 42a99befd1c2
+Revises: c2f2b99745e8
+Create Date: 2024-10-31 11:17:10.896060
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '42a99befd1c2'
+down_revision = 'c2f2b99745e8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/c2f2b99745e8_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""merge multiple heads
+
+Revision ID: c2f2b99745e8
+Revises: 7514fcb21c1d
+Create Date: 2024-10-31 11:08:09.835144
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c2f2b99745e8'
+down_revision = '7514fcb21c1d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
